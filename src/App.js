0fbe8478ad28407966f617c8c054f0997d6466e3
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-23 15:38:24 -0500

    part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'
+import contacts from "./contacts.json";
 import './App.css';
 
+
+
 function App() {
+  let fiveArr = contacts.slice(0, 5);
+  let remainArr = contacts.slice(5, contacts.length);
+
+  const [celebs, setCelebs] = React.useState(fiveArr);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <table style={{ border: "1px solid black" }}>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Action</th>
+        </tr>
+
+        {celebs.map((props, i) => {
+          return (
+            <tr key={i}>
+              <td>
+                <img style={{ width: "100px" }} src={props.pictureUrl} />
+              </td>
+              <td>
+                <p>{props.name}</p>
+              </td>
+              <td>
+                <p>{Math.round(props.popularity * 100) / 100}</p>
+              </td>
+              <td>
+                <p>{props.wonOscar ? "🏆" : ""}</p>
+              </td>
+              <td>
+                <p>{props.wonEmmy ? "🏆" : ""}</p>
+              </td>
+              <td>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
